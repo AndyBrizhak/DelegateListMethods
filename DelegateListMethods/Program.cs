8954a34142b12901ab2b5e9f8aac16e0236ffda3
Andrey Brizhak <andrewbrizhak@gmail.com>
2018-01-28 19:17:28 +0300

    add Write array, add Sort array

--- DelegateListMethods/Program.cs
+++ DelegateListMethods/Program.cs
@@ -6,6 +6,40 @@ using System.Threading.Tasks;
 
 namespace DelegateListMethods
 {
+    delegate void OpStroke(ref int[] arr);
+
+    public class ArrOperation
+    {
+        public static void WriteArray(ref int[] arr)
+        {
+            Console.WriteLine("Исходный массив: ");
+            foreach (int i in arr)
+                Console.Write("{0}\t", i);
+            Console.WriteLine();
+        }
+
+        // Сортировка массива
+        public static void IncSort(ref int[] arr)
+        {
+            int j, k;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                j = 0;
+                do
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        k = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = k;
+                    }
+                    j++;
+                }
+                while (j < arr.Length - 1);
+            }
+
+         }
+
     class Program
     {
         static void Main(string[] args)
